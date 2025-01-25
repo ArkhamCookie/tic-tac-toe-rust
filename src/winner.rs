b4bf647c31d0f5318abffaf206c34976bd930a00
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-01-25 14:38:26 -0600

    style: sort differently

--- src/winner.rs
+++ src/winner.rs
@@ -8,14 +8,14 @@ pub struct WinnerData {
 
 #[derive(Debug, PartialEq)]
 pub enum Line {
+	ZeroDiagonal,
+	OneDiagonal,
 	ZeroHorizontal,
 	OneHorizontal,
 	TwoHorizontal,
 	ZeroVertical,
 	OneVertical,
 	TwoVertical,
-	ZeroDiagonal,
-	OneDiagonal,
 	None,
 }
 
@@ -26,23 +26,90 @@ pub enum Winner {
 	None,
 }
 
-/// Check **all postions** for a winner
-pub fn check_winner(board: Board) -> WinnerData {
-	let mut winner_data: WinnerData;
+/// Check a line diagonally
+fn check_line_diagonal(board: &Board, line: u8) -> WinnerData {
+	match line {
+		0 => {
+			let slots = board.get_slots();
 
-	winner_data = check_lines_horizontal(&board);
-	if winner_data.winner != Winner::None {
-		return winner_data;
+			if board.slot_available(0) {
+				return WinnerData {
+					winner: Winner::None,
+					line: Line::None,
+				};
+			}
+
+			if slots[0] == slots[4] && slots[0] == slots[8] {
+				match slots[0] {
+					Slot::PlayerOne => {
+						return WinnerData {
+							winner: Winner::PlayerOne,
+							line: Line::ZeroDiagonal,
+						};
+					}
+					Slot::PlayerTwo => {
+						return WinnerData {
+							winner: Winner::PlayerTwo,
+							line: Line::ZeroDiagonal,
+						};
+					}
+					_ => {}
+				}
+			}
+
+			WinnerData {
+				winner: Winner::None,
+				line: Line::None,
+			}
+		}
+		1 => {
+			let slots = board.get_slots();
+
+			if board.slot_available(2) {
+				return WinnerData {
+					winner: Winner::None,
+					line: Line::None,
+				};
+			}
+
+			if slots[2] == slots[4] && slots[2] == slots[6] {
+				match slots[2] {
+					Slot::PlayerOne => {
+						return WinnerData {
+							winner: Winner::PlayerOne,
+							line: Line::OneDiagonal,
+						};
+					}
+					Slot::PlayerTwo => {
+						return WinnerData {
+							winner: Winner::PlayerTwo,
+							line: Line::OneDiagonal,
+						};
+					}
+					_ => {}
+				}
+			}
+
+			WinnerData {
+				winner: Winner::None,
+				line: Line::None,
+			}
+		}
+		_ => WinnerData {
+			winner: Winner::None,
+			line: Line::None,
+		},
 	}
+}
 
-	winner_data = check_lines_vertical(&board);
-	if winner_data.winner != Winner::None {
-		return winner_data;
-	}
-
-	winner_data = check_lines_diagonal(&board);
-	if winner_data.winner != Winner::None {
-		return winner_data;
+/// Check **all** lines diagonally
+fn check_lines_diagonal(board: &Board) -> WinnerData {
+	// Loop through all lines to check
+	for i in 0..2 {
+		let winner_data = check_line_diagonal(board, i);
+		if winner_data.winner != Winner::None {
+			return winner_data;
+		}
 	}
 
 	WinnerData {
@@ -301,90 +368,23 @@ fn check_lines_vertical(board: &Board) -> WinnerData {
 	}
 }
 
-/// Check a line diagonally
-fn check_line_diagonal(board: &Board, line: u8) -> WinnerData {
-	match line {
-		0 => {
-			let slots = board.get_slots();
+/// Check **all postions** for a winner
+pub fn check_winner(board: Board) -> WinnerData {
+	let mut winner_data: WinnerData;
 
-			if board.slot_available(0) {
-				return WinnerData {
-					winner: Winner::None,
-					line: Line::None,
-				};
-			}
-
-			if slots[0] == slots[4] && slots[0] == slots[8] {
-				match slots[0] {
-					Slot::PlayerOne => {
-						return WinnerData {
-							winner: Winner::PlayerOne,
-							line: Line::ZeroDiagonal,
-						};
-					}
-					Slot::PlayerTwo => {
-						return WinnerData {
-							winner: Winner::PlayerTwo,
-							line: Line::ZeroDiagonal,
-						};
-					}
-					_ => {}
-				}
-			}
-
-			WinnerData {
-				winner: Winner::None,
-				line: Line::None,
-			}
-		}
-		1 => {
-			let slots = board.get_slots();
-
-			if board.slot_available(2) {
-				return WinnerData {
-					winner: Winner::None,
-					line: Line::None,
-				};
-			}
-
-			if slots[2] == slots[4] && slots[2] == slots[6] {
-				match slots[2] {
-					Slot::PlayerOne => {
-						return WinnerData {
-							winner: Winner::PlayerOne,
-							line: Line::OneDiagonal,
-						};
-					}
-					Slot::PlayerTwo => {
-						return WinnerData {
-							winner: Winner::PlayerTwo,
-							line: Line::OneDiagonal,
-						};
-					}
-					_ => {}
-				}
-			}
-
-			WinnerData {
-				winner: Winner::None,
-				line: Line::None,
-			}
-		}
-		_ => WinnerData {
-			winner: Winner::None,
-			line: Line::None,
-		},
+	winner_data = check_lines_horizontal(&board);
+	if winner_data.winner != Winner::None {
+		return winner_data;
 	}
-}
 
-/// Check **all** lines diagonally
-fn check_lines_diagonal(board: &Board) -> WinnerData {
-	// Loop through all lines to check
-	for i in 0..2 {
-		let winner_data = check_line_diagonal(board, i);
-		if winner_data.winner != Winner::None {
-			return winner_data;
-		}
+	winner_data = check_lines_vertical(&board);
+	if winner_data.winner != Winner::None {
+		return winner_data;
+	}
+
+	winner_data = check_lines_diagonal(&board);
+	if winner_data.winner != Winner::None {
+		return winner_data;
 	}
 
 	WinnerData {
